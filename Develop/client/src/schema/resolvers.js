2929327289a5867eacbe4c5f1dab04c592ffd49a
jpcreativeworks <jp.graphics.011@gmail.com>
2022-05-18 21:13:18 -0700

    created type defs and started reslovers

---
+++ Develop/client/src/schema/resolvers.js
@@ -0,0 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { Book } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, arg, context) => {
+            if (context.user) {
+                const userData = await User.findOne({_id: context.user._id});
+                return userData;
+            }
+            throw new AuthenticationError('Whoops! Wrong User');
+        }
+    },
+    Mutation: {
+
+    }
+}
